c22a505ebaebf1da1c3b8a763cb37766652514bf
matheusgarcia37 <matheusgarcia37@hotmail.com>
2022-02-03 18:52:53 -0300

    att

---
+++ back/src/database/models/RegistroDeCidadao.ts
@@ -0,0 +1,44 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
+import { Denuncia } from './Denuncia'
+
+@Entity()
+export class RegistroDeCidadao {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  nome: string;
+
+  @Column()
+  email: string;
+
+  @Column({ type: 'float' })
+  telefone: number;
+
+  @Column()
+  cep: number
+
+  @Column()
+  endereco: string
+
+  @Column()
+  numero: number
+
+  @Column()
+  bairro: string
+  
+  @Column()
+  complemento: string
+
+  @OneToMany(type => Denuncia, registroDeCidadao => RegistroDeCidadao)
+  denuncias: Denuncia
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
